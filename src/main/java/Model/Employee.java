5e016b54b6e33bec976d65323705610bc311d7c5
Luc Perin Panta <lucpanta@MacBook-Pro-de-Luc.local>
2022-02-20 10:58:44 +0100

    adding Serializer to serialise data

--- src/main/java/Model/Employee.java
+++ src/main/java/Model/Employee.java
@@ -1,4 +1,13 @@
 package Model;
 
-public class Employee {
+import java.io.Serializable;
+
+public class Employee implements Serializable {
+    private Long id ;
+    private String name ;
+    private String email;
+    private String jobTitle;
+    private String phone;
+    private String imageUrl;
+    private String employeeCode;
 }
