a5366609259498b62ac7039ccf5fff20f151d681
Luc Perin Panta <lucpanta@MacBook-Pro-de-Luc.local>
2022-02-21 10:28:43 +0100

    ending  methode orELSETHTROW not working

--- src/main/java/EmployeeRepository/EmployeeRepo.java
+++ src/main/java/EmployeeRepository/EmployeeRepo.java
@@ -5,4 +5,7 @@ import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface EmployeeRepo extends JpaRepository<Employee ,Long> {
 
+    void deleteEmployeeById(Long id);
+
+    Employee findEmployeeById(Long id);
 }
--- src/main/java/Service/EmployeeService.java
+++ src/main/java/Service/EmployeeService.java
@@ -1,4 +1,38 @@
 package Service;
 
+import EmployeeRepository.EmployeeRepo;
+import Exceptions.UserNotFoundException;
+import Model.Employee;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.nio.file.attribute.UserPrincipalNotFoundException;
+import java.util.List;
+import java.util.UUID;
+
+@Service
 public class EmployeeService {
+    private final EmployeeRepo employeeRepo;
+
+    @Autowired
+    public EmployeeService(EmployeeRepo employeeRepo) {
+        this.employeeRepo = employeeRepo;
+    }
+    public Employee addEmployee(Employee employee){
+        employee.setEmployeeCode(UUID.randomUUID().toString());
+        return employeeRepo.save(employee);
+    }
+    public List<Employee> findAllEmployee(){
+        return employeeRepo.findAll();
+    }
+    public Employee updateEmployee(Employee employee){
+        return employeeRepo.save(employee);
+    }
+    public Employee findEmployeebyId(Long id ){
+         return employeeRepo.findEmployeeById(id);
+    }
+    public void deleteEmployee(Long id){
+         employeeRepo.deleteEmployeeById(id);
+    }
+
 }
